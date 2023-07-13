258155fa853ccef6ace5b1753f8977f845201471
theOGCodeWitcher <rahulsingh97r@gmail.com>
2023-07-13 17:00:22 -0400

    resolved conflicts

---
+++ front-end/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+   Profile Page
+    </div>
+  );
+};
+
+export default Profile;
