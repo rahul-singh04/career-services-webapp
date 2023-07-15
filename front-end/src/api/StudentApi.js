de6ef264013ebf1a40775f132d6f902d6cfe5d32
theOGCodeWitcher <rahulsingh97r@gmail.com>
2023-07-15 18:14:05 -0400

    Profile Section Integration with backend Issue #51

---
+++ front-end/src/api/StudentApi.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export const updateProfile = async (formData, authToken) => {
+  const headers = {
+    'x-access-token': authToken,
+  };
+
+  try {
+        const response = await axios.put('http://localhost:8090/api/test/candidate/updateUserProfile', formData, { headers });
+        console.log('Data successfully updated in the database:', response.data);
+        return response.data;
+    } catch (error) {
+        console.error('Error updating data in the database:', error);
+    }
+};
+
+
+export const getProfile = async (authToken) => {
+    const headers = {
+      'x-access-token': authToken,
+    };
+  
+    try {
+        const response = await axios.get('http://localhost:8090/api/test/candidate/getUserProfile', { headers });
+        return response.data;
+    } catch (error) {
+        console.error('Error in Fetching', error);
+    }
+  };
+  
