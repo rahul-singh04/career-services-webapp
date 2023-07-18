1d72aee11a80ab21682ace2a55829f44075c7e53
theOGCodeWitcher <rahulsingh97r@gmail.com>
2023-07-18 18:32:46 -0400

    Front end for edit and delete a job for employer

--- front-end/src/components/JobCardEmp.jsx
+++ front-end/src/components/JobCardEmp.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { getPhoto } from '../api/CommonApis';
 import defaultProfile from '../assets/defaultProfile.png'
-import { Button } from "@material-tailwind/react";
 import { Fragment } from "react";
 import {
-  Dialog, DialogHeader, IconButton,
+  Dialog
 } from "@material-tailwind/react";
-import JobApplyForm from '../components/JobApplyForm';
+import {
+  Drawer,
+  Button,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import ListAppliedCand from './ListAppliedCand';
+import EditJob from './EditJob';
 
 const JobCardEmp = ({ id, jobTitle, companyName, location, dateAdded, jobDescription, workLocation, totalOpenings }) => {
 
@@ -18,6 +23,10 @@ const JobCardEmp = ({ id, jobTitle, companyName, location, dateAdded, jobDescrip
   const handleOpen = () => setOpen(!open);
 
   const [photo, setPhoto] = useState(null);
+
+  const [openEdit, setopenEdit] = useState(false);
+  const openDrawer = () => setopenEdit(true);
+  const closeDrawer = () => setopenEdit(false);
 
   useEffect(() => {
     const authToken = JSON.parse(localStorage.getItem('user')).accessToken;
@@ -36,41 +45,60 @@ const JobCardEmp = ({ id, jobTitle, companyName, location, dateAdded, jobDescrip
   const handleApply = () => {
     handleOpen();
   };
+
   return (
     <Fragment>
-    <div className="flex flex-row bg-white rounded-lg shadow-md p-6 mb-4 gap-4">
-      <div className="flex items-center">
-        <img src={photo || defaultProfile} alt={companyName} className="w-24 h-24 rounded-full border-2 border-gray-200 mr-2 object-cover" />
-      </div>
-      <div className="flex flex-col w-full">
-        <h3 className="text-xl font-bold text-gray-800">{jobTitle}</h3>
-        <p className="text-blue-600 text-xs font-medium mb-2">{companyName}</p>
-        <p className="text-gray-600 text-sm mb-4">{jobDescription}</p>
-        <div className="flex items-center justify-between">
-          <div className='flex gap-2'>
-            <p className="text-blue-500 text-xs">{location}</p>
-            <p className="text-blue-500 text-xs">({workLocation})</p>
+      <div className="flex flex-row bg-white rounded-lg shadow-md p-6 mb-4 gap-4">
+        <div className="flex items-center">
+          <img src={photo || defaultProfile} alt={companyName} className="w-24 h-24 rounded-full border-2 border-gray-200 mr-2 object-cover" />
+        </div>
+        <div className="flex flex-col w-full">
+          <div className='flex justify-between'>
+            <h3 className="text-xl font-bold text-gray-800">{jobTitle}</h3>
+            <div className='flex gap-2'>
+            <Button size="sm" color="orange" className=" p-2" onClick={openDrawer}>
+              Edit
+            </Button>
+            <Button size="sm" color="deep-orange" className=" p-2" onClick={openDrawer}>
+              Delete
+            </Button>
+            </div>
           </div>
-          <p className="text-gray-600 text-xs">Posted on: <span className='text-blue-500'>{fullDate}</span></p>
-        </div>
-        <div className='flex justify-between my-1'>
-          <div className="bg-gray-100 my-2 p-2 w-fit rounded-md">
-            <p className="text-xs  text-gray-600">Total Openings: <span className="text-gray-700 font-semibold">{totalOpenings}</span></p>
+          <p className="text-blue-600 text-xs font-medium mb-2">{companyName}</p>
+          <p className="text-gray-600 text-sm mb-4">{jobDescription}</p>
+          <div className="flex items-center justify-between">
+            <div className='flex gap-2'>
+              <p className="text-blue-500 text-xs">{location}</p>
+              <p className="text-blue-500 text-xs">({workLocation})</p>
+            </div>
+            <p className="text-gray-600 text-xs">Posted on: <span className='text-blue-500'>{fullDate}</span></p>
           </div>
-          <Button size="sm" onClick={handleApply} variant="gradient" className='h-8 my-2'>Show Applicants</Button>
+          <div className='flex justify-between my-1'>
+            <div className="bg-gray-100 my-2 p-2 w-fit rounded-md">
+              <p className="text-xs  text-gray-600">Total Openings: <span className="text-gray-700 font-semibold">{totalOpenings}</span></p>
+            </div>
+            <Button size="sm" onClick={handleApply} variant="gradient" className='h-8 my-2'>Show Applicants</Button>
+          </div>
         </div>
+        <Dialog open={open} handler={handleOpen} >
+          <div className='flex justify-end'>
+            <IconButton variant="text" size="sm" onClick={handleOpen}>
+              <XMarkIcon className="h-4 w-4 " />
+            </IconButton>
+          </div>
+          <div className='mx-4 mb-8 px-8'>
+            <ListAppliedCand id={id} />
+          </div>
+        </Dialog>
+        <Drawer open={openEdit} onClose={closeDrawer} className="p-4" size={550}>
+          <div className="mb-6 flex items-center justify-end">
+            <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
+              <XMarkIcon strokeWidth={2} className="h-5 w-5" />
+            </IconButton>
+          </div>
+          <EditJob onUpdateSuccess={closeDrawer} />
+        </Drawer>
       </div>
-      <Dialog open={open} handler={handleOpen} style={{ maxHeight: "90vh" }}>
-        <div className='flex justify-end'>
-          <IconButton variant="text" size="sm" onClick={handleOpen}>
-            <XMarkIcon className="h-4 w-4 " />
-          </IconButton>
-        </div>
-        <div className='mx-4 mb-8 px-8'>
-            <ListAppliedCand id={id}/>
-        </div>
-      </Dialog>
-    </div>
     </Fragment>
   );
 };
