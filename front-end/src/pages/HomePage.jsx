dd53e273083b88447ff7e6b32c3a1b0b13a220f3
nithin <nithinreddy1013@gmail.com>
2023-07-29 01:15:06 -0400

    updated componenets and services

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -17,38 +17,162 @@ const HomePage = () => {
 
   return (
   
-  <HomeComponent>
-    <Title>
-      <P>Find your <span>[ Dream Job ]</span>, Find your Passion.</P>
-    </Title>
-    <Sides>
-      <LeftSide>
-        <Image>
+  // <HomeComponent>
+  //   <Title>
+  //     <P>Find your <span>[ Dream Job ]</span>, Find your Passion.</P>
+  //   </Title>
+  //   <Sides>
+  //     <LeftSide>
+  //       <Image>
           
-        </Image>
-      </LeftSide>
-      <RightSide>
-        <Subhead>
-          <p>Helping you Prepare,</p>
-          <p>For the World of Work.</p>
-          <h2></h2>
-        </Subhead>
+  //       </Image>
+  //     </LeftSide>
+  //     <RightSide>
+  //       <Subhead>
+  //         <p>Helping you Prepare,</p>
+  //         <p>For the World of Work.</p>
+  //         <h2></h2>
+  //       </Subhead>
 
-        <Explore>
-            <button onClick={handleClick} >Explore More</button>
-        </Explore>
-        <BP> <p>Student Support, Carrer / Co-Op and Employment, Resume Builder, Job-Fairs, Market Research</p> 
-        <h2></h2>
-        </BP>
-        <Contect>
-          <h4>Let the right people know you’re open to work</h4>
-          <h2>Share you resume and let the world know a new talent is in the hunt. </h2>
-          <h3>All at one place with simple clicks. </h3>
-        </Contect>
+  //       <Explore>
+  //           <button onClick={handleClick} >Explore More</button>
+  //       </Explore>
+  //       <BP> <p>Student Support, Carrer / Co-Op and Employment, Resume Builder, Job-Fairs, Market Research</p> 
+  //       <h2></h2>
+  //       </BP>
+  //       <Contect>
+  //         <h4>Let the right people know you’re open to work</h4>
+  //         <h2>Share you resume and let the world know a new talent is in the hunt. </h2>
+  //         <h3>All at one place with simple clicks. </h3>
+  //       </Contect>
 
-      </RightSide>
-    </Sides>
-    <Cards ref={ref}>
+  //     </RightSide>
+  //   </Sides>
+  //   <Cards ref={ref}>
+  //     <Cleft>
+  //       <H>
+  //         <h2>Find the right Job or</h2>
+  //         <h2>Internship for you</h2>
+  //       </H>
+  //     </Cleft>
+  //     <Cright>
+  //         <CRTitle>
+  //           <p>Suggested Searches</p> 
+  //         </CRTitle>
+  //         <Line1>
+  //             <Tile1>
+  //               <button>Engineering</button>
+  //             </Tile1>
+  //             <Tile2>
+  //               <button>Business Development</button>
+  //             </Tile2>
+  //             <Tile3>
+  //               <button>Marketing</button>
+  //             </Tile3>
+  //             <Tile4>
+  //               <button>Finance</button>
+  //             </Tile4>
+  //         </Line1>
+  //         <Line2>
+  //             <Tile1>
+  //               <button>Software Development</button>
+  //             </Tile1>
+  //             <Tile2>
+  //               <button>Customer Service</button>
+  //             </Tile2>
+  //             <Tile3>
+  //               <button>Industrial Design</button>
+  //             </Tile3>
+  //             <Tile4>
+  //               <button>Customer Service</button>
+  //             </Tile4>
+  //         </Line2>
+  //         <Line2>
+  //             <Tile1>
+  //                 <button>Retail Associate</button>
+  //             </Tile1>
+  //             <Tile2>
+  //               <button>Arts & Design</button>
+  //             </Tile2>
+  //             <Tile3>
+  //               <button>Product Management</button>
+  //             </Tile3>
+  //             <Tile4>
+  //               <button>Media & Communications</button>
+  //             </Tile4>
+  //         </Line2>
+  //         <Line2>
+  //             <Tile1>
+  //                 <button>Support</button>
+  //             </Tile1>
+  //             <Tile2>
+  //               <button>HealthCare Service</button>
+  //             </Tile2>
+  //             <Tile3>
+  //               <button>Consulting</button>
+  //             </Tile3>
+  //             <Tile4>
+  //               <button>Purchasing</button>
+  //             </Tile4>
+  //         </Line2>
+  //     </Cright>
+  //   </Cards>
+  //   <Carrer>
+  //     <About>
+  //         <p>The Career Hub provides career and employment (permanent/summer/ part-time) support services for undergraduate, graduate students and alumni from all disciplines. The Hub helps students develop lifelong career management skills through a comprehensive range of accessible services to support and empower students in making informed decisions about their career and employment goals. The Hub also assists employers in recruiting students from the university.</p>
+  //     </About>
+  //     <Services>
+  //       <h3>Services Include</h3>
+  //       <S>
+  //         <button>Job Postings</button>
+  //         <button>On-Campus Jobs</button>
+  //         <button>Internship Opportunities</button>
+  //         <button>Career Fairs</button>
+  //         <button>Career Coaching</button>
+  //         <button>Resume, Cover Letter, and Interview Advising</button>
+  //         <button>Employment and Career Workshops</button>
+  //       </S>
+  //     </Services>
+  //     <DropDowns>
+  //         <T>
+  //           <p>Recent Career Fairs</p>
+  //         </T>
+  //         <DD>
+  //           <D1>
+  //             <p>Career Fair (2022 - 2023)</p>
+  //           </D1>
+  //           <D1>
+  //             <p>Career Fair (2021 - 2022)</p>
+  //           </D1>
+  //           <D1>
+  //             <p>Career Fair (2020 - 2021)</p>
+  //           </D1>
+  //         </DD>
+  //     </DropDowns>
+  //   </Carrer>
+  //   <Footer>
+
+  //   </Footer>
+  // </HomeComponent>
+
+    <Container>
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional community</h1>
+          <img src="/images/login-hero.svg" alt="" />
+        </Hero>
+        <Form>
+          <Google onClick={handleClick} >
+            <img src="/images/google.svg" alt="" />
+            Explore More
+          </Google>
+          <Subtext>
+          <p>Helping you Prepare, For the World of Work.</p>
+          </Subtext>
+        </Form>
+      </Section>
+      <Sectiontwo>
+      <Cards ref={ref}>
       <Cleft>
         <H>
           <h2>Find the right Job or</h2>
@@ -117,7 +241,9 @@ const HomePage = () => {
           </Line2>
       </Cright>
     </Cards>
-    <Carrer>
+      </Sectiontwo>
+      <Sectionthree>
+      <Carrer>
       <About>
           <p>The Career Hub provides career and employment (permanent/summer/ part-time) support services for undergraduate, graduate students and alumni from all disciplines. The Hub helps students develop lifelong career management skills through a comprehensive range of accessible services to support and empower students in making informed decisions about their career and employment goals. The Hub also assists employers in recruiting students from the university.</p>
       </About>
@@ -133,31 +259,167 @@ const HomePage = () => {
           <button>Employment and Career Workshops</button>
         </S>
       </Services>
-      <DropDowns>
-          <T>
-            <p>Recent Career Fairs</p>
-          </T>
-          <DD>
-            <D1>
-              <p>Career Fair (2022 - 2023)</p>
-            </D1>
-            <D1>
-              <p>Career Fair (2021 - 2022)</p>
-            </D1>
-            <D1>
-              <p>Career Fair (2020 - 2021)</p>
-            </D1>
-          </DD>
-      </DropDowns>
+     
     </Carrer>
     <Footer>
 
     </Footer>
-  </HomeComponent>
+      </Sectionthree>
+    </Container>
+
   );
 };
 
 export default HomePage;
+const Container = styled.div`
+  padding:0px;
+  /* font-family: sans-serif; */
+
+`;
+const Section = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+
+
+const Sectiontwo = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 300px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+
+const Sectionthree = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 300px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+const Hero = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+
+  img {
+    /* z-index: -1; */
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      position: initial;
+      height: initial;
+    }
+  }
+`;
+
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+  /* border: 2px solid red; */
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  background-color: #fff;
+  align-items: center;
+  height: 56px;
+  width: 100%;
+  font-family: sans-serif;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+    inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0);
+
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
+  }
+`;
+
+const Subtext = styled.div`
+  padding-top: 80px;
+  font-size: 24px;
+  display: flex;
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const Carrer = styled.div`
     display: flex;
@@ -190,7 +452,6 @@ const Services = styled.div`
   justify-content:center;
   align-items: center;
 `;
-const DropDowns = styled.div``;
 const S = styled.div`
   display: flex;
   flex-direction: row;
@@ -212,171 +473,12 @@ const S = styled.div`
   }
   
 `;
-const T = styled.div``;
-
-const DD = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-`;
-const D1 = styled.div`
-
-  p{
-    border: 2px solid red;
-    padding: 20px;
-  }
-`;
 
 
 
 
-
-
-
-
-
-
-
-const HomeComponent = styled.div`
-  display: flex;
-  flex-direction:column ;
-  background:#f2f2f2 ;
-  border-top: 2px solid #5B0101;
-`;
-
-const Title = styled.div`
-  /* flex: 0.4; */
-  padding: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const P = styled.div`
-  
-  font-weight: 900;
-  font-size: 4rem;
-  font-family: 'Oswald', sans-serif;
-  color: #5B0101;
-  /* font-family: Montserrat; */
-  /* font-size: 64px; */
-  font-style: normal;
-  /* font-weight: 800; */
-  /* line-height: 70.9%; 45.376px */
-  letter-spacing: -4.64px;
-
-  >span{
-    color: #073f07;
-    font-size: 4rem;
-    font-family: 'Dancing Script', cursive;
-    letter-spacing: -5.64px;
-  }
-`;
-
-const Sides = styled.div`
-  /* flex: 0.6; */
-  display: flex;
-  flex-direction:row;
-  height: 70vh;
-  justify-content:space-around ;
-`;
-
-const LeftSide = styled.div`
-  flex: 0.4;
-  border:4px solid #5B0101 ;
-  border-radius: 30px;
-  height: 70vh;
-  display: flex;
-  justify-content:center ;
-  align-items:center ;
-
-`;
-const Image = styled.div`
-  background: url("/img.png");
-  background-size: 70vh 70vh;
-  padding: 20px;
-  display:flex ;
-  height: 100%;
-  width: 100%;
-  background-repeat:no-repeat ;
-  justify-content:center ;
-  align-items:center ;
-`;
-
-const RightSide = styled.div`
-  flex: 0.5;
-  /* border: 2px solid green; */
-  height: 70vh;
-  display: flex;
-  flex-direction:column ;
-  /* justify-content:center ; */
-  /* align-items:center ; */
-  
-`;
-const BP = styled.div`
-display: flex;
-justify-content: center;
-flex-direction:column ;
-padding:10px ;
-  p{
-    font-weight:900 ;
-    font-size:1rem ;
-  }
-  h2{
-    height: 2px;
-    width: 45vw;
-    background: black;
-  }
-`;
-
-const Contect = styled.div`
-h4{
-  font-size: 2.1rem;
-}
-  h2{
-    /* font-family: 'Caveat', cursive; */
-    font-size: 1.2rem;
-  }
-  h3{
-    /* font-family: 'Caveat', cursive; */
-    font-size: 1.2rem;
-    text-decoration:  underline solid  black 2px;
-  }
-`;
-
-const Explore = styled.div`
-padding-top: 10px;
-  >button{
-    
-    color: #5B0101;
-    padding: 15px;
-    font-family: 'Oswald', sans-serif;
-    font-weight:500 ;
-    font-size: 2rem;
-    border-radius: 20px;
-    border:2px solid #5B0101;
-  }
-  >button:hover{
-    color: white;
-    background: #5B0101;
-  }
-`;
-
-const Subhead = styled.div`
-    padding-top: 2rem;
-    font-size: 3rem;
-    font-weight: 800;
-    font-family: 'Oswald', sans-serif;
-    h2{
-      border-bottom: 6px solid black;
-      width: 35vw;
-    }
-`;
 
 const Cards = styled.div`
-  height: 50vh;
   /* border:2px solid red ; */
   display: flex;
   justify-content:space-around ;
@@ -390,22 +492,21 @@ const H = styled.div`
   padding: 20px;
   flex-direction:column ;
   h2{
-    font-size:2.5rem ;
+    font-size:2rem ;
     font-weight: 900;
-    font-family: 'Oswald', sans-serif;
-    color: #000000;
+    
+    color: #2977c9;
 
   }
 `;
 
-
 const Cleft = styled.div`
-  flex: 0.25;
+  /* flex: 0.45; */
   display: flex; 
   /* border:2px solid black ; */
 `
 const Cright = styled.div`
-  flex: 0.5;
+  /* flex: 0.7; */
   display: flex;
   /* border:2px solid green ; */
   flex-direction: column;
@@ -423,8 +524,6 @@ padding-bottom:20px ;
   }
 `;
 
-
-
 const Line1 = styled.div`
   display: flex;
   justify-content:center ;
@@ -441,70 +540,57 @@ const Line2 = styled.div`
 `;
 
 const Tile1 = styled.div`
-  /* border: 2px solid #ffffff; */
   border-radius: 10px;
   padding-right:5px;
   height: 3rem;
   cursor: pointer;
-  >button{
-    background: #5B0101;
-  }
-  
-  color: #ffffff;
+  background: #ffffff;
+  /* box-shadow: black .5px .5px; */
+  color: #2977c9;
   :hover{
-    color: #ffffff;
-    background: #014a1e;
+    color: #ffffff; 
+    background: #2977c9;
   }
 `;
 const Tile2 = styled.div`
-   /* border: 2px solid #ffffff; */
-  border-radius: 10px;
+   border-radius: 10px;
   padding-right:5px;
   height: 3rem;
   cursor: pointer;
-  >button{
-    background: #5B0101;
-  }
-  
-  color: #ffffff;
+  background: #ffffff;
+  /* box-shadow: black .5px .5px; */
+  color: #2977c9;
   :hover{
-    color: #ffffff;
-    background: #014a1e;
+    color: #ffffff; 
+    background: #2977c9;
   }
 `;
 const Tile3 = styled.div`
-   /* border: 2px solid #ffffff; */
-  border-radius: 10px;
+   border-radius: 10px;
   padding-right:5px;
   height: 3rem;
   cursor: pointer;
-  >button{
-    background: #5B0101;
-  }
-  
-  color: #ffffff;
+  background: #ffffff;
+  /* box-shadow: black .5px .5px; */
+  color: #2977c9;
   :hover{
-    color: #ffffff;
-    background: #014a1e;
+    color: #ffffff; 
+    background: #2977c9;
   }
 `;
 const Tile4 = styled.div`
-   /* border: 2px solid #ffffff; */
-  border-radius: 10px;
+   border-radius: 10px;
   padding-right:5px;
   height: 3rem;
   cursor: pointer;
-  >button{
-    background: #5B0101;
-  }
-  
-  color: #ffffff;
+  background: #ffffff;
+  /* box-shadow: black .5px .5px; */
+  color: #2977c9;
   :hover{
-    color: #ffffff;
-    background: #014a1e;
+    color: #ffffff; 
+    background: #2977c9;
   }
 `;
-
 
 const Footer = styled.div`
   height: 15vh;
