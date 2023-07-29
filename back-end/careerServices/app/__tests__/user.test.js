9cb35dcbe41985c48d072550fff38217ad861c81
VigneshP1505 <vickynsp15@gmail.com>
2023-07-29 17:11:51 -0400

    added get all jobs,candidate profile,filter jobs unit test suites

--- back-end/careerServices/app/__tests__/user.test.js
+++ back-end/careerServices/app/__tests__/user.test.js
@@ -1,10 +1,11 @@
-const { browseCandidates } = require("../controllers").userController
+const { browseCandidates, browseJobs, getUserProfile } = require("../controllers").userController
 const { userService } = require("../services")
-const { userModel } = require("../models")
 
 jest.mock("../services/user-service/user.service", () => ({
     getFilteredCandidates: jest.fn(),
-    getAllJobs: jest.fn()
+    getAllJobs: jest.fn(),
+    browseJobs: jest.fn(),
+    getUserProfile: jest.fn()
 }));
 
 const mockResponse = () => {
@@ -14,8 +15,7 @@ const mockResponse = () => {
     return res;
 };
 
-describe("browse all candidates test suite", () => {
-
+describe("browse candidates", () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
@@ -47,5 +47,106 @@ describe("browse all candidates test suite", () => {
         expect(res.status).toHaveBeenCalledWith(500);
         expect(res.send).toHaveBeenCalledWith('Failed to fetch candidates');
     });
-
 })
+
+describe('Get all jobs', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('should fetch all jobs and respond with status 200', async () => {
+        // Mock the userService.getAllJobs function to return sample job data
+        const sampleJobs = [
+            {
+                _id: '1',
+                jobTitle: 'Software Engineer',
+                jobDesc: 'Job description goes here.',
+                fullName: 'John Doe',
+                companyLocation: 'New York, USA',
+                workLocation: 'Remote',
+                totalOpenings: 5,
+                datePosted: '2023-07-29',
+                employerID: {
+                    username: 'john_doe',
+                    email: 'john@example.com',
+                    fullName: 'John Doe',
+                },
+            },
+        ];
+        userService.getAllJobs.mockResolvedValue(sampleJobs);
+
+        const req = {};
+        const res = mockResponse();
+
+        await browseJobs(req, res);
+
+        expect(userService.getAllJobs).toHaveBeenCalledTimes(1);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.send).toHaveBeenCalledWith(sampleJobs);
+    });
+
+    test('should handle error and respond with status 500', async () => {
+        // Mock the userService.getAllJobs function to throw an error
+        const errorMessage = 'Database connection failed.';
+        userService.getAllJobs.mockRejectedValue(new Error(errorMessage));
+
+        const req = {};
+        const res = mockResponse();
+
+        await browseJobs(req, res);
+
+        expect(userService.getAllJobs).toHaveBeenCalledTimes(1);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.send).toHaveBeenCalledWith('Failed to fetch jobs');
+    });
+});
+
+
+describe('Get UserProfile', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('should fetch user profile and respond with status 200', async () => {
+        const sampleProfile = {
+            _id: '1',
+            username: 'john_doe',
+            email: 'john@example.com',
+            fullName: 'John Doe',
+        };
+        userService.getUserProfile.mockResolvedValue(sampleProfile);
+
+        const req = {
+            headers: {
+                'x-access-token': 'sample-access-token',
+            },
+        };
+        const res = mockResponse();
+
+        await getUserProfile(req, res);
+
+        expect(userService.getUserProfile).toHaveBeenCalledTimes(1);
+        expect(userService.getUserProfile).toHaveBeenCalledWith('sample-access-token');
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.send).toHaveBeenCalledWith(sampleProfile);
+    });
+
+    test('should handle error and respond with status 500', async () => {
+        const errorMessage = 'Database connection failed.';
+        userService.getUserProfile.mockRejectedValue(new Error(errorMessage));
+
+        const req = {
+            headers: {
+                'x-access-token': 'invalid-access-token',
+            },
+        };
+        const res = mockResponse();
+
+        await getUserProfile(req, res);
+
+        expect(userService.getUserProfile).toHaveBeenCalledTimes(1);
+        expect(userService.getUserProfile).toHaveBeenCalledWith('invalid-access-token');
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.send).toHaveBeenCalledWith('Failed to fetch profile');
+    });
+});
