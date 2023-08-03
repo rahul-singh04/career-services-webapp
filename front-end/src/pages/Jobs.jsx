743d31035ea324f6235381e5919c8796a961dabb
theOGCodeWitcher <rahulsingh97r@gmail.com>
2023-08-03 17:01:37 -0400

    search functionality added #125

--- front-end/src/pages/Jobs.jsx
+++ front-end/src/pages/Jobs.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import JobCard from '../components/JobCard';
 import { getAllJobs } from '../api/StudentApi';
-
-
+import { Input } from "@material-tailwind/react";
 
 const JobList = () => {
 
-  console.log("testing CI/CD deployment")
-
   const [jobs, setJobs] = useState();
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const authToken = JSON.parse(localStorage.getItem('user')).accessToken;
@@ -21,22 +19,42 @@ const JobList = () => {
       })
   }, [])
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  const filteredJobs = jobs?.filter((job) =>
+  job.jobTitle.toLowerCase().includes(searchTerm.toLowerCase())
+   || (job.employerID.fullName && job.employerID.fullName.toLowerCase().includes(searchTerm.toLowerCase()))
+);
+console.log(jobs);
+
 
   return (
     <div className="w-1/2 mx-auto">
       <h2 className="text-2xl font-bold m-4 text-center">Job Listings</h2>
-      {jobs && jobs.map((job) => (
+      <div className="flex justify-center mb-4">
+        <div className='w-72'>
+          <Input
+            label="Search Jobs"
+            icon={<i class="fa-brands fa-searchengin fa-lg"  />}
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+      </div>
+      {filteredJobs && filteredJobs.map((job) => (
         <JobCard
           key={job._id}
           id={job._id}
           jobTitle={job.jobTitle}
-          companyName={job.fullName}
+          companyName={job.employerID.fullName}
           location={job.companyLocation}
           dateAdded={job.datePosted}
           workLocation={job.workLocation}
           totalOpenings={job.totalOpenings}
           jobDescription={job.jobDesc}
-          companyId={job.employerID._id}
+          companyId={job.employerID._id }
         />
       ))}
     </div>
