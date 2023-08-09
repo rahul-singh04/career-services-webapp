32ed63205d33cd7c523ffdee5a08777a4416afc3
VigneshP1505 <vickynsp15@gmail.com>
2023-08-09 15:22:08 -0400

    cors allowed for all origins

--- back-end/careerServices/app/config/server.config.js
+++ back-end/careerServices/app/config/server.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   url: "mongodb+srv://root:root1234@cluster0.9dtcyul.mongodb.net/careerServices-db",
   secret: "careerServices-secret-key",
-  corsOrigin: "http://localhost:5173",
+  corsOrigin: "*",
   serverPort: 8090,
 };
